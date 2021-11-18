e8b03675244899bac7b2b88115df29c0cce3b9ab
Yash Desai <yhdesai99@ymail.com>
2021-11-18 15:31:27 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -9,13 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOH5oIBrFhHtDsmh1YbnRHhSsS-FVL6yU",
-  authDomain: "makers-community.firebaseapp.com",
-  projectId: "makers-community",
-  storageBucket: "makers-community.appspot.com",
-  messagingSenderId: "153192757247",
-  appId: "1:153192757247:web:7e64e2dc096133508dd9ca",
-  measurementId: "G-MM4W9DX6RP"
+
 };
 
 // Initialize Firebase
